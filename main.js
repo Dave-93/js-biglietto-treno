214edb9098608e23dc55befa89409d587a2a529e
Dave-93 <difrancesco.davide93@gmail.com>
2024-12-10 21:21:47 +0100

    fix: switch order number and remove excess console.log

--- main.js
+++ main.js
@@ -1,28 +1,19 @@
 let inputKm = prompt("Inserisci i Km che desideri percorrere");
 let inputAge = prompt("Inserisci la tua età");
-const scontoUnder18 = (inputKm * 0.21) * 20 / 100;
-const scontoOver65 = (inputKm * 0.21) * 40 / 100;
-
-let outputPrice;
-
-// console.log(typeof inputKm, inputKm);
-// console.log(typeof inputAge, inputAge);
-
 inputKm = Number(inputKm);
 inputAge = Number(inputAge);
 
-// console.log(typeof inputKm, inputKm);
-// console.log(typeof inputAge, inputAge);
+const scontoUnder18 = (inputKm * 0.21) * 20 / 100;
+const scontoOver65 = (inputKm * 0.21) * 40 / 100;
+let outputPrice;
 
 if(isNaN(inputKm) || isNaN(inputAge)){
     console.log("Ricarica la pagina ed inserisci i dati in formato numerico!");
 } else if(inputAge < 18){
     outputPrice = (inputKm * 0.21) - scontoUnder18;
-    console.log(`Il costo del biglietto è ${outputPrice.toFixed(2).replace(".",",")} €`);
 }else if(inputAge > 65){
     outputPrice = (inputKm * 0.21) - scontoOver65;
-    console.log(`Il costo del biglietto è ${outputPrice.toFixed(2).replace(".",",")} €`);
 } else{
     outputPrice = (inputKm * 0.21)
-    console.log(`Il costo del biglietto è ${outputPrice.toFixed(2).replace(".",",")} €`);
 }
+console.log(`Il costo del biglietto è ${outputPrice.toFixed(2).replace(".",",")} €`);
